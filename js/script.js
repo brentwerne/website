e8581ab441fb5be2467b522892253845a324e06b
Brent Werne <bwerne2018@fau.edu>
2022-08-30 20:10:27 -0400

    finished

--- js/script.js
+++ js/script.js
@@ -18,75 +18,172 @@ playerMarginIncrement = parseInt(document.getElementById('player-falling-blocks'
 document.addEventListener('keyup', (event) => {
    
     let character = document.getElementById('player-falling-blocks');
-
-    if (event.code == 'ArrowLeft')
+    if (gamePlaying)
     {
-        let characterLeft = parseInt(character.style.marginLeft);
-        
-        if (characterLeft - playerMarginIncrement >= 0)
+        if (event.code == 'ArrowLeft')
         {
-            playerMargin -= playerMarginIncrement;
+            let characterLeft = parseInt(character.style.marginLeft);
+            
+            if (characterLeft - playerMarginIncrement >= 0)
+            {
+                playerMargin -= playerMarginIncrement;
+            }
+            else
+            {
+                playerMargin = document.getElementById('game-frame').clientWidth - character.clientWidth - playerMarginIncrement;
+            }
         }
-        else
+
+        if (event.code == 'ArrowRight')
         {
-            playerMargin = document.getElementById('game-frame').clientWidth - character.clientWidth - playerMarginIncrement;
+            let characterRight = parseInt(character.style.marginLeft) + character.clientWidth;
+
+            if (characterRight + playerMarginIncrement <= document.getElementById('game-frame').clientWidth)
+            {
+                playerMargin += playerMarginIncrement;
+            }
+            else
+            {
+                playerMargin = playerMarginIncrement;
+            }
         }
     }
-
-    if (event.code == 'ArrowRight')
-    {
-        let characterRight = parseInt(character.style.marginLeft) + character.clientWidth;
-
-        if (characterRight + playerMarginIncrement <= document.getElementById('game-frame').clientWidth)
-        {
-            playerMargin += playerMarginIncrement;
-        }
-        else
-        {
-            playerMargin = playerMarginIncrement;
-        }
-    }
-
     document.getElementById('player-falling-blocks').style.marginLeft = playerMargin;
 });
 
-let gameover = false;
+let scoreCount = 0;
+
+let gamePlaying = false;
+
+let playButtonBool = true;
+let playButton = document.getElementById("image-button");
+playButton.addEventListener("click", (event) => {
+
+    if (playButtonBool)
+    {
+        gamePlaying = true;
+
+        createFallingBlock();
+        blockFalls();
+        checkForCollision();
+        gameIsOver();
+
+        playButton.setAttribute ("src", "assets/img/pause_button.png");
+
+        playButtonBool = false;
+    }
+    else
+    {
+
+        playButton.setAttribute ("src", "assets/img/play_button.png");
+
+        playButtonBool = true;
+
+        gamePlaying = false;
+    }
+
+
+});
+
+
+let gameOver = false;
 let lives = 1;
 let generatorInterval = 4;
+let blockCounter = 0;
+
+let fallingblocks = [];
 
 async function createFallingBlock () {
 
-    let node = document.createElement("div");
-    node.setAttribute("id", "falling-block");
+    if (gamePlaying && !gameOver)
+    {
+        let node = document.createElement("div");
+        node.setAttribute("class", "falling-block");
+        node.setAttribute("id", "falling-block"+blockCounter);
+        blockCounter++;
+        node.style.width  = document.getElementById('player-falling-blocks').clientWidth;
+        node.style.height = document.getElementById('player-falling-blocks').clientWidth;
+        
+        node.style.marginLeft = Math.floor(Math.random() * (parseInt(document.getElementById("game-frame").clientWidth) - parseInt(node.style.width)));
+        node.style.marginTop = parseInt(document.getElementById("game-frame").clientHeight) * (-.95);
+        document.getElementById("game-frame").appendChild (node);
 
-    node.style.width  = document.getElementById('player-falling-blocks').clientWidth;
-    node.style.height = document.getElementById('player-falling-blocks').clientWidth;
-    
-    node.style.marginLeft = Math.floor(Math.random() * (parseInt(document.getElementById("game-frame").clientWidth) - parseInt(node.style.width)));
-    node.style.marginTop = parseInt(document.getElementById("game-frame").clientHeight) * (-.95);
-    document.getElementById("game-frame").appendChild (node);
+        fallingblocks.push(node);
 
-    setTimeout(createFallingBlock, generatorInterval*250);
+        setTimeout(createFallingBlock, generatorInterval*100);
+    }
 }
 
-async function blocksFall()
+async function checkForCollision()
 {
-    if (document.getElementById("falling-block").marginTop == null)
+    if (gamePlaying && !gameOver)
     {
-        document.getElementById("falling-block").marginTop = parseInt(document.getElementById("falling-block").style.marginTop);
-    }
-    document.getElementById("falling-block").marginTop += parseInt(document.getElementById('player-falling-blocks').clientWidth);
-    
-    document.getElementById("falling-block").style.marginTop = document.getElementById("falling-block").marginTop;
-    
-    if (parseInt(document.getElementById("falling-block").style.marginTop) >= 0 )
-    {
-        let node = document.getElementById("falling-block");
-        document.getElementById("game-frame").removeChild(node);
+        console.log (-1 * parseInt(document.getElementById('player-falling-blocks').clientHeight));
+        console.log (parseInt(fallingblocks[0].style.marginTop));
+        if (parseInt(fallingblocks[0].style.marginTop) > (-1 * parseInt(document.getElementById('player-falling-blocks').clientHeight)))
+        {
+            let playerLeft  = parseInt(document.getElementById('player-falling-blocks').style.marginLeft);
+            let playerRight = parseInt(document.getElementById('player-falling-blocks').style.marginLeft) + parseInt(document.getElementById('player-falling-blocks').clientWidth);
+
+            let blockLeft = parseInt(fallingblocks[0].style.marginLeft);
+            let blockRight = parseInt(fallingblocks[0].style.marginLeft) + parseInt(fallingblocks[0].style.width);
+            
+            if ( blockLeft > playerLeft && blockLeft < playerRight ||
+                blockRight > playerLeft && blockRight < playerRight)
+            {
+                gameOver = true;
+                gamePlaying = false;
+            }
+        }
     }
 
-    setTimeout(blocksFall, 500)
+    setTimeout (checkForCollision, 1);
 }
 
-createFallingBlock();
-blocksFall();
+async function removeBlock(node)
+{
+    if (parseInt(node.style.marginTop) >= 0 )
+    {
+        document.getElementById("game-frame").removeChild(node);
+        fallingblocks.shift();
+        scoreCount++;
+        document.getElementById("score-count").innerHTML = "Score Count: " + scoreCount;
+    }
+}
+
+async function blockFalls()
+{
+    if (gamePlaying && !gameOver)
+    {
+        for (let i = 0; i < fallingblocks.length; i++)
+        {
+            fallingblocks[i].style.marginTop = parseInt(fallingblocks[i].style.marginTop) + parseInt(fallingblocks[i].clientWidth);
+                
+            removeBlock(fallingblocks[i]);
+        }
+
+        setTimeout(blockFalls, 125);
+    }
+}
+
+
+async function gameIsOver ()
+{
+    if (gameOver)
+    {
+        alert("Game over :(\nYou scored " + scoreCount + " points")
+        for (let i = 0; i < fallingblocks.length; i++)
+        {
+            document.getElementById("game-frame").removeChild(fallingblocks[i]);
+        }
+        fallingblocks = [];
+
+        playButton.setAttribute ("src", "assets/img/play_button.png");
+
+        playerMargin = parseInt(document.getElementById('player-falling-blocks').style.marginLeft);
+        
+        gameOver = false;
+    }
+
+    setTimeout(gameIsOver, 500);
+}
